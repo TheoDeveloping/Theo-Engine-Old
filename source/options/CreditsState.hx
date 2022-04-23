5049b87132f72f08d3f371c856d480a2530b6d6e
TheoDeveloper <97988283+TheoPortz@users.noreply.github.com>
2022-04-23 14:22:38 -0600

    Add files via upload

--- source/options/CreditsState.hx
+++ source/options/CreditsState.hx
@@ -1,4 +1,4 @@
-package options;
+package;
 
 #if desktop
 import Discord.DiscordClient;
@@ -12,6 +12,7 @@ import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
 import lime.utils.Assets;
 import ui.FlxVirtualPad;
 
@@ -19,32 +20,21 @@ using StringTools;
 
 class CreditsState extends MusicBeatState
 {
-	var curSelected:Int = 1;
+	var curSelected:Int = -1;
 
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private var iconArray:Array<AttachedSprite> = [];
-
-	private static var creditsStuff:Array<Dynamic> = [ //Name - Icon name - Description - Link - BG Color
-		['FnF Android Team'],
-		['LuckyDog', 'luckydog', 'Original FnF Android Creator', 'https://twitter.com/Shadow_Mario_', 0xFFFFDD33],
-		['ZacksGamerz', 'zack', 'v.0.2.8 Coder', 'https://twitter.com/Shadow_Mario_', 0xFFFFDD33],
-		['Theo Engine Team'],
-		['ThesPorts', 'theo', 'Programmer Of Theo Engine',	 'https://twitter.com/river_oaken',	0xFFC30085],
-		['EstoyAburridow', 'aburridow', 'Code Helper', 'https://twitter.com/river_oaken', 0xFFC30085],
-		['ElArabe98', 'arabe', 'Logo Bumpin Creator', 'https://twitter.com/river_oaken', 0xFFC30085],
-		["Funkin Crew"],
-		['NinjaMuffin99', 'muffin', "Programmer Of Friday Night Funkin'", 'https://twitter.com/ninja_muffin99',	0xFFF73838],
-		['PhantomArcade', 'phantom', "Animator Of Friday Night Funkin'", 'https://twitter.com/PhantomArcade3K',	0xFFFFBB1B],
-		['EvilSk8r', 'evil', "Artist Of Friday Night Funkin'", 'https://twitter.com/evilsk8r', 0xFF53E52C],
-		['KawaiSprite', 'kawaii', "Composer Of Friday Night Funkin'", 'https://twitter.com/kawaisprite', 0xFF6475F3]
-	];
+	private var creditsStuff:Array<Array<String>> = [];
 
 	var bg:FlxSprite;
 	var descText:FlxText;
 	var intendedColor:Int;
 	var colorTween:FlxTween;
-
+	var descBox:AttachedSprite;
+	
 	var _pad:FlxVirtualPad;
+
+	var offsetThing:Float = -75;
 
 	override function create()
 	{
@@ -53,27 +43,54 @@ class CreditsState extends MusicBeatState
 		DiscordClient.changePresence("In the Menus", null);
 		#end
 
+		persistentUpdate = true;
 		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
 		add(bg);
-
+		bg.screenCenter();
+		
 		grpOptions = new FlxTypedGroup<Alphabet>();
 		add(grpOptions);
 
+		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
+		['FnF Android Team'],
+		['LuckyDog', 'luckydog', 'Original FnF Android Creator', 'https://youtube.com/channel/UCeHXKGpDKo2eqYKVkqCUdaA', 0xFFD700],
+		['ZacksGamerz', 'zack', 'v.0.2.8 Coder', 'https://youtube.com/channel/UCbWNOpUvvruwi3pbYVC_yWQ', 0x008000],
+		['Theo Engine Team'],
+		['ThesPorts', 'theo', 'Programmer Of Theo Engine',	 'https://youtube.com/c/TheoDeveloper', 0x191970],
+		['EstoyAburridow', 'aburridow', 'Code Helper', 'https://twitter.com/EstoyAburridop', 0x1E90FF],
+		['ElArabe98', 'arabe', 'Logo Bumpin Creator', 'https://www.youtube.com/channel/UCi_xw_cLbN_T6xoFYqKlAeA',0x696969 ],
+		["Funkin Crew"],
+		['NinjaMuffin99', 'muffin', "Programmer Of Friday Night Funkin'", 'https://twitter.com/ninja_muffin99',	0xFFF73838],
+		['PhantomArcade', 'phantom', "Animator Of Friday Night Funkin'", 'https://twitter.com/PhantomArcade3K',	0xFFFFBB1B],
+		['EvilSk8r', 'evil', "Artist Of Friday Night Funkin'", 'https://twitter.com/evilsk8r', 0xFF53E52C],
+		['KawaiSprite', 'kawaii', "Composer Of Friday Night Funkin'", 'https://twitter.com/kawaisprite', 0xFF6475F3]
+		];
+		
+		for(i in pisspoop){
+			creditsStuff.push(i);
+		}
+	
 		for (i in 0...creditsStuff.length)
 		{
 			var isSelectable:Bool = !unselectableCheck(i);
 			var optionText:Alphabet = new Alphabet(0, 70 * i, creditsStuff[i][0], !isSelectable, false);
 			optionText.isMenuItem = true;
 			optionText.screenCenter(X);
+			optionText.yAdd -= 70;
 			if(isSelectable) {
 				optionText.x -= 70;
 			}
-			optionText.x = optionText.x;
+			optionText.forceX = optionText.x;
 			//optionText.yMult = 90;
 			optionText.targetY = i;
 			grpOptions.add(optionText);
 
 			if(isSelectable) {
+				if(creditsStuff[i][5] != null)
+				{
+					Paths.currentModDirectory = creditsStuff[i][5];
+				}
+
 				var icon:AttachedSprite = new AttachedSprite('credits/' + creditsStuff[i][1]);
 				icon.xAdd = optionText.width + 10;
 				icon.sprTracker = optionText;
@@ -81,57 +98,124 @@ class CreditsState extends MusicBeatState
 				// using a FlxGroup is too much fuss!
 				iconArray.push(icon);
 				add(icon);
+				Paths.currentModDirectory = '';
+
+				if(curSelected == -1) curSelected = i;
 			}
 		}
+		
+		descBox = new AttachedSprite();
+		descBox.makeGraphic(1, 1, FlxColor.BLACK);
+		descBox.xAdd = -10;
+		descBox.yAdd = -10;
+		descBox.alphaMult = 0.6;
+		descBox.alpha = 0.6;
+		add(descBox);
 
-		descText = new FlxText(50, 600, 1180, "", 32);
-		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		descText = new FlxText(50, FlxG.height + offsetThing - 25, 1180, "", 32);
+		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER/*, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK*/);
 		descText.scrollFactor.set();
-		descText.borderSize = 2.4;
+		//descText.borderSize = 2.4;
+		descBox.sprTracker = descText;
 		add(descText);
 
-		bg.color = creditsStuff[curSelected][4];
+		bg.color = getCurrentBGColor();
 		intendedColor = bg.color;
 		changeSelection();
+
+        #if android
+        addVirtualPad(UP_DOWN, A_B);
+        #end
+        
+		_pad = new FlxVirtualPad(UP_DOWN, A);
+		_pad.alpha = 0.75;
+		this.add(_pad);
+
 		super.create();
 	}
 
+	var quitting:Bool = false;
+	var holdTime:Float = 0;
 	override function update(elapsed:Float)
 	{
-		FlxG.sound.playMusic(Paths.music('creditSong'), 0);
-
 		if (FlxG.sound.music.volume < 0.7)
 		{
 			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
 		}
 
-		if (controls.UP_P)
-			changeSelection(-1);
-		if (controls.DOWN_P)
-			changeSelection(1);
-
-		if (controls.BACK)
+		if(!quitting)
 		{
-			if(colorTween != null) {
-				colorTween.cancel();
+			if(creditsStuff.length > 1)
+			{
+				var shiftMult:Int = 1;
+				if(FlxG.keys.pressed.SHIFT) shiftMult = 3;
+
+		    	var UP_PAD = _pad.buttonUp.justPressed;
+		    	var DOWN_PAD = _pad.buttonDown.justPressed;
+		     	var BACK = _pad.buttonB.justPressed;
+		    	var ACCEPT = _pad.buttonA.justPressed;
+
+		    	if (UP_PAD)
+				{
+					changeSelection(-1 * shiftMult);
+					holdTime = 0;
+				}
+				if (DOWN_PAD)
+				{
+					changeSelection(1 * shiftMult);
+					holdTime = 0;
+				}
+
+				if(controls.UI_DOWN || controls.UI_UP)
+				{
+					var checkLastHold:Int = Math.floor((holdTime - 0.5) * 10);
+					holdTime += elapsed;
+					var checkNewHold:Int = Math.floor((holdTime - 0.5) * 10);
+
+					if(holdTime > 0.5 && checkNewHold - checkLastHold > 0)
+					{
+						changeSelection((checkNewHold - checkLastHold) * (controls.UI_UP ? -shiftMult : shiftMult));
+					}
+				}
 			}
-			FlxG.sound.play(Paths.sound('cancelMenu'));
-			FlxG.switchState(new MainMenuState());
-		}
-		if (controls.ACCEPT) {
-			CoolUtil.browserLoad(creditsStuff[curSelected][3]);
-		}
-			#if mobileC
-			addVirtualPad(UP_DOWN, A_B);
-			#end
 
-			_pad = new FlxVirtualPad(UP_DOWN, A_B);
-			_pad.alpha = 0.75;
-			this.add(_pad);
-
+			if(ACCEPT) {
+				CoolUtil.browserLoad(creditsStuff[curSelected][3]);
+			}
+			if (BACK)
+			{
+				if(colorTween != null) {
+					colorTween.cancel();
+				}
+				FlxG.sound.play(Paths.sound('cancelMenu'));
+				MusicBeatState.switchState(new MainMenuState());
+				quitting = true;
+			}
+		}
+		
+		for (item in grpOptions.members)
+		{
+			if(!item.isBold)
+			{
+				var lerpVal:Float = CoolUtil.boundTo(elapsed * 12, 0, 1);
+				if(item.targetY == 0)
+				{
+					var lastX:Float = item.x;
+					item.screenCenter(X);
+					item.x = FlxMath.lerp(lastX, item.x - 70, lerpVal);
+					item.forceX = item.x;
+				}
+				else
+				{
+					item.x = FlxMath.lerp(item.x, 200 + -40 * Math.abs(item.targetY), lerpVal);
+					item.forceX = item.x;
+				}
+			}
+		}
 		super.update(elapsed);
 	}
 
+	var moveTween:FlxTween = null;
 	function changeSelection(change:Int = 0)
 	{
 		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
@@ -143,7 +227,7 @@ class CreditsState extends MusicBeatState
 				curSelected = 0;
 		} while(unselectableCheck(curSelected));
 
-		var newColor:Int = creditsStuff[curSelected][4];
+		var newColor:Int =  getCurrentBGColor();
 		if(newColor != intendedColor) {
 			if(colorTween != null) {
 				colorTween.cancel();
@@ -154,16 +238,6 @@ class CreditsState extends MusicBeatState
 					colorTween = null;
 				}
 			});
-
-			var UP_P = _pad.buttonUp.justPressed;
-			var DOWN_P = _pad.buttonDown.justPressed;
-	
-			var ACCEPT = _pad.buttonA.justPressed;
-			var BACK = _pad.buttonB.justPressed;
-	
-			#if android
-				var BACK = _pad.buttonB.justPressed || FlxG.android.justReleased.BACK;
-			#end
 		}
 
 		var bullShit:Int = 0;
@@ -180,7 +254,23 @@ class CreditsState extends MusicBeatState
 				}
 			}
 		}
+
 		descText.text = creditsStuff[curSelected][2];
+		descText.y = FlxG.height - descText.height + offsetThing - 60;
+
+		if(moveTween != null) moveTween.cancel();
+		moveTween = FlxTween.tween(descText, {y : descText.y + 75}, 0.25, {ease: FlxEase.sineOut});
+
+		descBox.setGraphicSize(Std.int(descText.width + 20), Std.int(descText.height + 25));
+		descBox.updateHitbox();
+	}
+
+	function getCurrentBGColor() {
+		var bgColor:String = creditsStuff[curSelected][4];
+		if(!bgColor.startsWith('0x')) {
+			bgColor = '0xFF' + bgColor;
+		}
+		return Std.parseInt(bgColor);
 	}
 
 	private function unselectableCheck(num:Int):Bool {
